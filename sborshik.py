b3ea9bcdb5a34b0226d751df450ae11b1f4d9e51
Алеесей <703355@mail.ru>
2022-11-08 01:03:02 +0500

    обнова

--- sborshik.py
+++ sborshik.py
@@ -1,6 +1,7 @@
-# проверка
 import requests
-import lxml
+
+import lxml.html
+
 from bs4 import BeautifulSoup
 import csv
 
@@ -11,44 +12,35 @@ HEADERS = {'accept': '*/*',
            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.124 YaBrowser/22.9.4.863 Yowser/2.5 Safari/537.36'
            }
 
-def get_html(url, params=''):
+def poluchaem_html(url, params=''):
     r = requests.get(url, headers=HEADERS, params=params)
     return r
 
-def get_ssylki(html):
+def poluchaem_ssylki(html):
     tree = lxml.html.document_fromstring(html.text)
     a_spis = tree.xpath("//div[@class='item_link']/a/@href")
     return a_spis
 
 def sbor_ssylok_so_vseh_stranic():
     vse_ssylki = []
-    for i in range(1, 1):
+    for i in range(1, 2):
         print(f'Обрабатывается страница {i}')
         ssylka = URL + "/" + str(i)
-        html = get_html(ssylka)
-        ssylki_na_tovary = get_ssylki(html)
+        html = poluchaem_html(ssylka)
+        ssylki_na_tovary = poluchaem_ssylki(html)
         vse_ssylki.extend(ssylki_na_tovary)
     vse_ssylki = list(set(vse_ssylki))
-    print(len(vse_ssylki))
+    return vse_ssylki
 
-def sbor_dannyh_tovara():
+def sbor_dannyh_tovara(ssylki, params=''):
     tree = lxml.html.document_fromstring(html.text)
-    dannye_tovara = []
-    dannye_tovara.append(
-        {
-            'артикул': tree.xpath("//span[@itemprop='sku']"),
-            'название': tree.xpath("//h1"),
-            'категория': tree.xpath("//div[@id='breadcrumps']/a/span"),
-            'цена': tree.xpath("//span[@class='item_cost']/text()"),
-            'описание': tree.xpath("//div[@id='description_box']/div[1]")
-        }
-    )
-    print(dannye_tovara)
+    r = requests.get(ssylki[0], headers=HEADERS, params=params)
+    print(r.status_code)
 
-
-html = get_html(URL)
-#ssylki = get_ssylki(html)
+html = poluchaem_html(URL)
 ssylki = sbor_ssylok_so_vseh_stranic()
+tovary = poluchaem_ssylki(html)
+sbor_dannyh = sbor_dannyh_tovara(ssylki)
 
-#print(ssylki)
+print(sbor_dannyh)
 
