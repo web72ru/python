bad027bd294dfe6279bbbd29fd90da794bce59b1
Алексей <703355@mail.ru>
2022-11-25 11:50:32 +0500

    актировка - выборка новостей

--- aktirovka.py
+++ aktirovka.py
@@ -5,7 +5,7 @@ disable_warnings(exceptions.InsecureRequestWarning)
 
 session = requests.Session()
 
-исходник = 'https://www.tyumen-city.ru/'
+исходник = 'https://www.tyumen-city.ru/sobitii/'
 шапка = {
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 YaBrowser/22.11.0.2419 Yowser/2.5 Safari/537.36'
@@ -14,21 +14,32 @@ session = requests.Session()
 
 def получаем_хтмл(ссылка, params=''):
     ответ = requests.get(ссылка, headers=шапка, params=params, verify=False)
-    хтмл_страницы = ответ.content.decode('windows-1251')
+    хтмл = ответ.content.decode('windows-1251')
 
-    return хтмл_страницы
+    return хтмл
 
 
 хтмл_страницы = получаем_хтмл(исходник)
 
 
-def находим_новость(хтмл_страницы):
-    икспас = lxml.html.document_fromstring(хтмл_страницы.text)
-    ссылки_на_новость = икспас.xpath('//a[@class="testt"]/@href')
+def находим_ссылки(страница):
+    икспас = lxml.html.document_fromstring(страница)
+    ссылки = икспас.xpath('//a[@class="testt"]/@href')
 
-    return ссылки_на_новость
+    return ссылки
 
 
-#ссылки_на_новость = находим_новость(хтмл_страницы)
+ссылки_на_новость = находим_ссылки(хтмл_страницы)
 
-print(хтмл_страницы)
+
+def выборка_новостей():
+    новости = []
+    for ссылка in ссылки_на_новость:
+        страница_новости = получаем_хтмл(ссылка)
+
+        print(страница_новости)
+
+    return новости
+
+
+print(выборка_новостей())
--- py.py
+++ py.py
@@ -1,22 +1,6 @@
-import requests
-import lxml.html
-from bs4 import BeautifulSoup
-import csv
-import re
+глобальная = 40
 
-CSV = 'loft4you.csv'
+def fun(локальная):
+    print(локальная)
 
-HOST = 'https://loft4you.ru/'
-URL = 'https://loft4you.ru/catalog/disaynerskie_svetilniki'
-HEADERS = {'accept': '*/*',
-           'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.124 YaBrowser/22.9.4.863 Yowser/2.5 Safari/537.36'
-           }
-
-
-def poluchaem_html(url, params=''):
-    r = requests.get(url, headers=HEADERS, params=params)
-    return r
-
-html = poluchaem_html(HOST)
-
-print(html)
+fun(глобальная)
